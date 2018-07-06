6f004fa473a6cb8f2ad5a0ad9260103d2af643f1
sudosudoo <35059872+sudosudoo@users.noreply.github.com>
2018-07-07 00:13:48 +1000

    change https to http

--- rip.py
+++ rip.py
@@ -4,7 +4,7 @@ import random
 
 while True:
     sites = random.choice(['google.com.au', 'www.bom.gov.au/?ref=logo', 'reddit.com/r/ooer', 'youtube.com'])
-    visit = "https://{}".format(sites)
+    visit = "http://{}".format(sites)
     webbrowser.open(visit)
     seconds = random.randrange(5, 20)
     time.sleep(seconds)
